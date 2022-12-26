d4c592c79c520b49228a4a8d8ec397fbd5bd7519
tsoodula <tarun.soodula@capgemini.com>
2022-12-26 17:06:52 +0530

    Adding the sql file

---
+++ one-to-many-bi.sql
@@ -0,0 +1,39 @@
+use hibernate_mappings; 
+drop table if exists instructor_detail;
+create table instructor_detail(
+	id int auto_increment not null,
+    youtube_channel varchar(128) default null,
+    hobby varchar(45) default null,
+    
+    primary key (id) 
+);
+
+drop table if exists instructor;
+create table instructor(
+	id int auto_increment not null,
+    first_name varchar(45) default null,
+    last_name varchar(45) default null,
+    email varchar(45) default null,
+    instructor_detail_id int default null,
+    
+    key fk_detail_idx (instructor_detail_id),
+    foreign key (instructor_detail_id) 
+    references instructor_detail (id), 
+    primary key (id) 
+);
+
+
+drop table if exists course;
+create table course(
+	id int not null auto_increment,
+    title varchar(128),
+    instructor_id int,
+    unique(title),
+    
+    primary key (id),
+    foreign key (instructor_id) references instructor(id)
+);
+
+
+
+
